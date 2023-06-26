b39cda853f48c896c39937721215c63c0440d489
Omar <ocamacho812@gmail.com>
2023-06-26 13:18:07 -0700

    Add technologies

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -2,10 +2,17 @@ import { BallCanvas } from './canvas'
 import { SectionWrapper } from '../higher-order-components'
 import { technologies } from '../constants'
 
+
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((tech, idx) => (
+        <div className='w-28 h-28' key={idx}>
+          <BallCanvas icon={tech.icon} />
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, 'tech')
